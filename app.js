ce9a474b06aa6116335f7f41d8b49fd0d219d58b
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 13:01:21 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Perímetro de un rectángulo')
+
+alert('Perímetro de un rectángulo')
+
+const ancho = prompt('Ingresa el valor de la ancho del rectángulo');
+const altura = prompt('Ingresa el valor de la altura del rectángulo');
+const perimetro = (2 * ancho) + (2 * altura);
+
+alert(`El perímetro del rectángulo es de ${perimetro}`);
